0075a719a4779248159121416c893ef951ecd70e
Daniel Allen <dan@aeternagroup.net>
2026-01-11 14:48:58 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "dns_srv_records" {
+  description = "All dns_srv_record resources"
+  value       = azurerm_dns_srv_record.dns_srv_records
+}
+
---
+++ variables.tf
@@ -0,0 +1,32 @@
+variable "dns_srv_records" {
+  description = <<EOT
+Map of dns_srv_records, attributes below
+Required:
+    - name
+    - resource_group_name
+    - ttl
+    - zone_name
+    - record (block):
+        - port (required)
+        - priority (required)
+        - target (required)
+        - weight (required)
+Optional:
+    - tags
+EOT
+
+  type = map(object({
+    name                = string
+    resource_group_name = string
+    ttl                 = number
+    zone_name           = string
+    tags                = optional(map(string))
+    record = object({
+      port     = number
+      priority = number
+      target   = string
+      weight   = number
+    })
+  }))
+}
+
